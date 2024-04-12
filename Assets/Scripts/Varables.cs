ba150ad9c3dd3fd990761f7f5b4fbd5207f76b91
ALix903 <sasha_ole@bk.ru>
2024-04-12 17:08:10 +0300

    Initial commit

---
+++ Assets/Scripts/Varables.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Varables : MonoBehaviour
+{
+    public int firstCount;
+    public float secondCount;
+    public bool thirdCount;
+    public string nameCity;
+    public GameObject house;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
